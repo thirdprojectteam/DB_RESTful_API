c6c52ec4793200625262a8c8a7f467c55d62cadb
newbie <thddpfla1107@gmail.com>
2025-08-09 20:52:05 +0900

    완성본

---
+++ DB_RESTful_API/main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include "datamanager.h"
+#include "apiserver.h"
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    DataManager::instance().connect("192.168.2.57", "bank", "master", "1107", 3306);
+    APIServer apis;
+    apis.start();
+    return a.exec();
+}
